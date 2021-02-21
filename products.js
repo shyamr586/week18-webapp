b01b7ecf94a0f8371098a2b4a3ed58345a8c6f17
shyamr586 <67099874+shyamr586@users.noreply.github.com>
2021-02-21 10:06:11 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,25 @@
+let products =  [
+    
+    {
+        "id": 1002,
+        "title": "Pet hamster",
+        "description":'<em>My</em> ham live in closet rent 10 dolla',
+        "price":5.99,
+        "img":'https://upload.wikimedia.org/wikipedia/commons/9/9e/Giraffe_Mikumi_National_Park.jpg',
+        "stock": 1,
+        "availableInventory": 1,
+        "rating": 1,
+    },
+
+    {
+        "id": 1001,
+        "title": "Pet rat",
+        "description":'<em>My</em> rat live in my basement rent free',
+        "price":3.00,
+        "img":'https://www.catster.com/wp-content/uploads/2015/06/cat-food-bowl-shutterstock_157112807_0.jpg',
+        "stock": 5,
+        "availableInventory": 5,
+        "rating": 3,
+    },
+
+];
