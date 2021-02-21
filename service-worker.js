4a703ce4e0c86ca637046bc34dc732b2e8867342
shyamr586 <67099874+shyamr586@users.noreply.github.com>
2021-02-21 11:02:59 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -7,35 +7,38 @@ var cacheFiles = [
     "cat.jpg",
 ];
 
-self.addEventListener("install", (e) =>{
+self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
-        caches.open(cacheName).then((cache) =>{
-            console.log('CACHING ALL FILES');
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
             return cache.addAll(cacheFiles);
         })
     );
 });
 
-// self.addEventListener('fetch', function(e){
+// self.addEventListener('fetch', function (e) {
 //     e.respondWith(
-//         caches.match(e.request).then(function (r){
-//             console.log("FETCHING RESOURCE"+e.request.url);
-//             return r
+//         // check if the cache has the file
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//             // 'r' is the matching file if it exists in the cache
+//             return r 
 //         })
-//     )
-// })
+//     );
+// });
 
-self.addEventListener('fetch', function(e){
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function (r){
-            console.log("FETCHING RESOURCE"+e.request.url);
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache, 
             return r || fetch(e.request).then(function (response) {
-                return caches.open(cacheName).then(function (cache){
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
                     cache.put(e.request, response.clone());
                     return response;
-                })
-            })
+                });
+            });
         })
-    )
-})
+    );
+});
